152f3e8aae91a46ea6bc0d42245ce4a844729288
AmandaACLucio <amandalucio@poli.ufrj.br>
2021-08-19 17:58:41 -0300

    BD

---
+++ Back/BD.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Users (
+    Id_User int not null,
+    User varchar(120),
+    Password varchar(120),
+    Name varchar(255),
+    Email varchar(255),
+    Poinsts int,
+    Instituicion varchar(255),
+    primary key(id_user)
+    
+);
