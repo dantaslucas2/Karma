84107cea4976e0e2b984f286f670c621768908f7
dantaslucas2 <32503769+dantaslucas2@users.noreply.github.com>
2021-08-19 20:16:16 -0300

    Create BD.sql

--- Back/BD.sql
+++ Back/BD.sql
@@ -1,11 +1,33 @@
-CREATE TABLE karmaschema.Users(
-    Id_User int not null,
-    User varchar(120),
-    Password varchar(120),
-    Name varchar(255),
-    Email varchar(255),
-    Points int,
-    Instituicion varchar(255),
-    primary key(id_user)
-    
+CREATE TABLE users (
+    id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(50),
+    points INT,
+    institution VARCHAR(100),
+    password VARCHAR(100),
+    user VARCHAR(100)
+);
+
+CREATE TABLE services (
+    id_service INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    tag VARCHAR(100),
+    points INT NOT NULL,
+    category VARCHAR(100),
+    description VARCHAR(200),
+    status INT,
+    type_service VARCHAR(200),
+    id_owner INT,
+    FOREIGN KEY (id_owner) REFERENCES users(id_user)
+);
+
+CREATE TABLE contracts (
+    id_contract INT AUTO_INCREMENT PRIMARY KEY,
+    date_begin DATE,
+    data_end DATE,
+    points INT,
+    id_service INT,
+    id_request INT,
+    FOREIGN KEY (id_service) REFERENCES services(id_service),
+    FOREIGN KEY (id_request) REFERENCES users(id_user)
 );
