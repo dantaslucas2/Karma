e21c28932e050b5d744f5a9018c0e7343ecb2ef6
AmandaACLucio <amandalucio@poli.ufrj.br>
2021-08-19 16:10:24 -0300

    Servindo Back

--- Back/index.js
+++ Back/index.js
@@ -1,1 +1,18 @@
-﻿
+﻿const  express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(4000,'0.0.0.0',()=>{
+    console.log("API RODANDO")
+})
+
+app.get('/', (req, res) => {
+    try {
+        res.send('Server running');
+    } catch {
+
+    }
+});
