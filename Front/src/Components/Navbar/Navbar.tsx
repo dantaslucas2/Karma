65fb68d430b73dbed85df9c6d8c30d9185ee1310
Namdlog <guilhermegoldman@poli.ufrj.br>
2021-08-21 16:27:16 -0300

    Add Navbar component

---
+++ Front/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,58 @@
+import { Component, useState }from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../../Assets/images/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+import { IoPersonCircleSharp } from "react-icons/io5";
+
+class Navbar extends Component<{}, { click: boolean, button: boolean}> {
+
+    constructor(props: any) {
+      super(props);
+      this.state = {
+        click: false,
+        button: true
+      };
+    }
+
+    handleClick = () => {
+      this.setState({click: !this.state.click})
+    }
+
+    closeMobileMenu = () => {
+      this.setState({click: false})
+    }
+
+    showButton = () => {
+      this.setState({button: window.innerWidth <= 960 ? false : true})
+    }
+
+
+    render(){
+      return (
+        <div className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo'>
+                <img src={logo} />
+            </Link>
+            <IconContext.Provider value={{ color: "white" }}>
+                <div className='menu-icon' onClick={this.handleClick}>
+                  {this.state.click ? <FaTimes /> : <FaBars />}
+                </div>
+              
+              <ul className={this.state.click ? 'nav-menu active': 'nav-menu'}>
+                <li className='nav-btn'>
+                  <Link to='/perfil' className='nav-link'>
+                    <IoPersonCircleSharp />
+                  </Link>
+                </li>
+              </ul>
+            </IconContext.Provider>
+          </div>
+        </div>
+      );
+    }
+}
+
+export default Navbar;
