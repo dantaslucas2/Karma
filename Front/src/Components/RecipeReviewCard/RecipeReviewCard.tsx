e919fc9a8c7a48da30ee1d28da4c746cbbc35825
Namdlog <guilhermegoldman@poli.ufrj.br>
2021-08-21 16:26:59 -0300

    Add RecipeReviewCard component

---
+++ Front/src/Components/RecipeReviewCard/RecipeReviewCard.tsx
@@ -0,0 +1,58 @@
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const MAX_LENGTH_DESCTIPTION = 255
+
+const compressMaxLength = (description: string) => {
+  if (description.length > MAX_LENGTH_DESCTIPTION) {
+    return description.substr(0, MAX_LENGTH_DESCTIPTION) + '...'
+  }
+  return description
+}
+
+export default function RecipeReviewCard(props: IPropCard) {
+
+  const card = props
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={card.title}
+        subheader={card.creationDate.toLocaleString("pt-BR")}
+      />
+     
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {compressMaxLength(card.description)}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
