1174dfab594b4ce543f0b613a869c2a6406b9c75
Namdlog <guilhermegoldman@poli.ufrj.br>
2021-08-21 16:25:58 -0300

    Add features in feed component

--- Front/src/Views/Feed/Feed.tsx
+++ Front/src/Views/Feed/Feed.tsx
@@ -1,29 +1,40 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { Component } from 'react';
 import './Feed.css';
-import Section from '../../Components/Section';
+import Section from '../../Components/Section/Section';
+import Navbar from '../../Components/Navbar/Navbar';
 
 const MockCategories: Array<IPropSection> = [
   {
-    title: "A",
+    title: "Programação",
     cards: [
-      {
-        title: "CardA",
-        description: "Dou aulas de Python"
-      },
+      { title: "Aulas de python", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", creationDate: new Date() },
+      { title: "Aulas de python", description: "Dou aulas de Python", creationDate: new Date() },
+      { title: "Aulas de python", description: "Dou aulas de Python", creationDate: new Date() },
+    ]
+  },
+  {
+    title: "Cálculo",
+    cards: [
+      { title: "Aulas de python", description: "Dou aulas de Python", creationDate: new Date() },
     ]
   }
 ]
 
-const Feed = () => {
+class Feed extends Component {
 
-  const Sections: any = () => MockCategories.map((category: IPropSection) => <Section {...category} />)
+  render() {
 
-  return (
-    <div className="Feed">
-      <Sections />
-    </div>
-  );
+    const Sections: any = () => MockCategories.map((category: IPropSection) => <Section {...category} />)
+
+    return (
+      <div className="Feed">
+        <Navbar />
+        <div className="FeedSections">
+          <Sections />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Feed;
