00a8bab137122f9d79696532a82dff756ca4b94a
AmandaACLucio <amandalucio@poli.ufrj.br>
2021-10-02 22:22:08 -0300

    fix authentication

--- Back/controllers/loginController.js
+++ Back/controllers/loginController.js
@@ -1,6 +1,8 @@
 const Users = require("../database/models/Users");
 const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
+const config = require('../config.json');
 
 
 async function authenticate(req, res) {
@@ -37,47 +39,60 @@ async function authenticate(req, res) {
 async function verifyUserLogin(user,password){
 
 
-/*   const userAuth = await Users.findOne({
+  const userLog = {user:user, password: password};
+
+  if (!(userLog.user && userLog.password)) {   
+    return res.status(400).send({ error: "Dados não estão no formato permitido" });
+  }
+
+  const userAuth = await Users.findOne({
     where: {
-      user: user,
+      user: userLog.user,
     }
-  }); */
+  });
 
-  res.status(200)
+  if(userAuth){
 
-  console.log("Aqui")
-/*   if(userAuth){
+    if (userAuth || bcrypt.compareSync(userLog.password, userAuth.password)) {
 
-    if (!userAuth || !bcrypt.compareSync(password, userAuth.password)) {
 
-      res.status(401).json({message: "Senha incorreta"});
-      return false;
+      console.log("entrei")
+      token = jwt.sign({id:userAuth.id_user,username:userAuth.user,type:'user'},config.secret,{ expiresIn: '2h'})
+      console.log("token", token)
+      return {status:'ok',data:token}
+      
     } else {
-      res.status(200).json({message: "Usuário Logado"});
-      return true;
+      return {status:'error',error:'invalid password'}
     }  
     
   }else{
     res.status(401).json({ error: "Usuário não existe" });
-
-  } */
+    return false;
+  }
 }
 
 async function login(req, res){
 
   const {user,password}=req.body;
 
+  console.log(req.body)
+
+
   const response = await verifyUserLogin(user,password);
+  const token = response.data
 
   console.log(response)
 
-/*   if(response.status==='ok'){
-    
+  if(response.status==='ok'){
+    console.log(token)    
     res.cookie('token',token,{ maxAge: 2 * 60 * 60 * 1000, httpOnly: true });  // maxAge: 2 hours
-    res.redirect('/feed');
+    res.redirect("/login");
+
   }else{
-      res.json(response);
-  } */
+
+    res.json(response);
+
+  }
 }
 
 
